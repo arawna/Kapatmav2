42f1ade7e18f906c4bdb245c2451fc8fff6d2c04
arawna <alihocaoglu1220@gmail.com>
2021-04-27 15:33:25 +0300

    Add project files.

---
+++ Kapatmav2/Kapatma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapatmav2
+{
+    class Kapatma
+    {
+        public void kapat()
+        {
+            Console.Write("Kapatma süresi giriniz(dk): ");
+            int sure = Convert.ToInt32(Console.ReadLine()) * 60;
+
+            System.Diagnostics.Process.Start("shutdown", "-f -f -f " + sure);
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Bilgisayarınız " + sure/60 + " dakika sonra kapatılacak.");
+            Console.Write("---------------");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kapatmav2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapatmav2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kapatma kapatma = new Kapatma();
+            baslangıc:
+            try
+            {
+                kapatma.kapat();
+            }
+            catch
+            {
+                Console.WriteLine("Sadece sayı giriniz!");
+                goto baslangıc;
+            }
+        }
+    }
+    
+}
